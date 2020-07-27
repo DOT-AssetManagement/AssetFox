9bcc557c2c8f1673ce816fddd8686d5f93253e88
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2020-07-27 14:35:51 -0500

    changed esec config for version 2 urls

--- BridgeCareApp/VuejsApp/src/config/oidc-config.ts
+++ BridgeCareApp/VuejsApp/src/config/oidc-config.ts
@@ -1,7 +1,7 @@
 const oidcConfig: any = {
     authorizationEndpoint: 'https://oidcservicessyst.penndot.gov/affwebservices/CASSO/oidc/BAMS/authorize',
     clientId: 'a56bb9d5-190d-4873-afea-0767d82dd91c',
-    redirectUri: 'https://iam-deploy.com/Authentication/'
+    redirectUri: 'https://v2.iam-deploy.com/Authentication/'
 };
 
 export default oidcConfig;
