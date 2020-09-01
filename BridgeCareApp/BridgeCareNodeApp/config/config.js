2a13344faec81735dc80b1f09a34ca1fcddcb432
Chadwick Becker <cbecker@ara.com>
2020-09-01 16:49:48 -0500

    Altered connectivity in each module to connect to iamvm. Need to discuss "Shared" functionality between the IR&D project and iAM for PennDOT.

--- BridgeCareApp/BridgeCareNodeApp/config/config.js
+++ BridgeCareApp/BridgeCareNodeApp/config/config.js
@@ -1,10 +1,11 @@
 module.exports = {
   development: {
-    db: 'mongodb://localhost:27017/BridgeCare?replicaSet=r1',
+    db: 'mongodb://admin:BridgecareARA123@52.188.172.73:27017/BridgeCare',
     port: process.env.PORT || 5000
   },
   production: {
-    db: 'mongodb://admin:BridgecareARA123@localhost:27017/BridgeCare?replicaSet=r1',
+    db: 'mongodb://admin:BridgecareARA123@52.188.172.73:27017/BridgeCare',
+    //db: 'mongodb://admin:BridgecareARA123@52.188.172.73:27017/BridgeCare?replicaSet=r1',
     port: process.env.PORT || 80
   }
 }
--- BridgeCareApp/BridgeCareNodeApp/config/mongoose.js
+++ BridgeCareApp/BridgeCareNodeApp/config/mongoose.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const debug = require('debug')('app:mongoose');
 
 module.exports = function(config) {
-
+    //mongoose.createConnection(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
     mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-      debug('connected to mongo db on the local');
+      debug('connected to mongo db at ' + config.db);
     })
     .catch((err) => {
-      debug('error has occured in connection');
+      debug('error has occured in connection: ' + err);
     });
+    debug('DB connection props: ' + config.db)
 };
