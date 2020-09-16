fee2a245b3a0ab89993da9bfb37dcc29de720ca7
sbhardwaj <sbhardwaj@ara.com>
2020-09-16 08:30:57 -0500

    changed config info for prod

--- BridgeCareApp/BridgeCareNodeApp/config/config.js
+++ BridgeCareApp/BridgeCareNodeApp/config/config.js
@@ -1,10 +1,11 @@
 module.exports = {
   development: {
-    db: 'mongodb://nbis_admin:BridgecareARA123@52.188.172.73:27017/NBIS_DEMO',
+    db: 'mongodb://admin:BridgecareARA123@localhost:27017/BridgeCare',
+    //db: 'mongodb://nbis_admin:BridgecareARA123@52.188.172.73:27017/NBIS_DEMO',
     port: process.env.PORT || 5000
   },
   production: {
-    db: 'mongodb://nbis_admin:BridgecareARA123@52.188.172.73:27017/NBIS_DEMO',
+    db: 'mongodb://nbis_admin:BridgecareARA123@localhost:27017/NBIS_DEMO',
     //db: 'mongodb://admin:BridgecareARA123@52.188.172.73:27017/BridgeCare?replicaSet=r1',
     port: process.env.PORT || 80
   }
