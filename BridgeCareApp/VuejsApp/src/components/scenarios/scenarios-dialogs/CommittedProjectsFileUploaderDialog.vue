609038389dc2280a508eb1f11024373f52f64bd3
ppatorno <ppatorno@ara.com>
2020-11-09 16:35:07 -0600

    -added a 'Delete' button to the committed projects dialog -added event handler functions for 'Delete' button click and alert submit/cancel response following confirmation of user's decision to delete/not delete committed projects -added service to create http delete request for given simulation's committed projects and also added an api endpoint -added function to delete a simulation's committed projects from the database -integrated delete function with the save committed projects function -wrapped all code paths inside authentication

--- BridgeCareApp/VuejsApp/src/components/scenarios/scenarios-dialogs/CommittedProjectsFileUploaderDialog.vue
+++ BridgeCareApp/VuejsApp/src/components/scenarios/scenarios-dialogs/CommittedProjectsFileUploaderDialog.vue
@@ -55,6 +55,12 @@
                     <v-btn @click="onUpload" class="ara-blue-bg white--text">Upload</v-btn>
                     <v-btn @click="onExport" class="ara-blue-bg white--text">Export</v-btn>
                     <v-btn @click="onCancel" class="ara-orange-bg white--text">Cancel</v-btn>
+                    <v-tooltip top>
+                        <template slot="activator">
+                            <v-btn @click="onDelete" class="ara-orange-bg white--text">Delete</v-btn>
+                        </template>
+                        <span>Delete scenario's current committed projects</span>
+                    </v-tooltip>
                 </v-layout>
             </v-card-actions>
         </v-card>
@@ -191,6 +197,13 @@
         onExport() {
             this.$emit('submit', null, true);
             this.files = [];
+        }
+
+        /**
+         * Emits the 'delete' event when 'Delete' button has been clicked
+         */
+        onDelete() {
+            this.$emit('delete');
         }
     }
 </script>
