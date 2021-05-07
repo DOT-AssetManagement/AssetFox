56435e958c581c017351f607561c3acd0fca4688
sbhardwaj <sbhardwaj@ara.com>
2021-05-07 14:54:51 -0500

    changed config.js

--- BridgeCareApp/BridgeCareNodeApp/config/config.js
+++ BridgeCareApp/BridgeCareNodeApp/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   development: {
-    db: 'mongodb://admin:BridgecareARA123@localhost:27017/BridgeCare',
+    //db: 'mongodb://admin:BridgecareARA123@localhost:27017/BridgeCare',
+    db: 'mongodb://localhost:27017/NBIS_DEMO',
     //db: 'mongodb://nbis_admin:BridgecareARA123@52.188.172.73:27017/NBIS_DEMO',
     port: process.env.PORT || 5000
   },
