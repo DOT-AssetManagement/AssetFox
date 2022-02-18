a585ba7947bd250ee3792101c582ad2132e6862a
William Rummler <w.a.rummler@gmail.com>
2022-02-18 15:22:08 -0600

    Document that SequentialTaskQueue only works with "cold" tasks.

--- BridgeCareApp/BridgeCareCore/Services/SequentialTaskQueue.cs
+++ BridgeCareApp/BridgeCareCore/Services/SequentialTaskQueue.cs
@@ -3,6 +3,11 @@ using System.Threading.Tasks;
 
 namespace BridgeCareCore.Services
 {
+    /// <summary>
+    ///     This queue does not support "hot" (i.e. started) tasks. If a task is added to this queue
+    ///     and is started prior to this queue's consumer task reaching it and starting it, this
+    ///     queue's consumer task will fail.
+    /// </summary>
     public class SequentialTaskQueue : BlockingCollection<Task>
     {
         private bool Disposed;
