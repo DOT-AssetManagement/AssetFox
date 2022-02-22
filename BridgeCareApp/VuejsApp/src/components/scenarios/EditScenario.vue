765d6abc7a492d9db5f49f7ba436b1acfd55fe07
Tyler Daws <tyler.j.daws@gmail.com>
2022-02-22 08:32:01 -0500

    Changed button visibility based on admin access

--- BridgeCareApp/VuejsApp/src/components/scenarios/EditScenario.vue
+++ BridgeCareApp/VuejsApp/src/components/scenarios/EditScenario.vue
@@ -257,7 +257,10 @@ export default class EditScenario extends Vue {
                             },
                         };
 
-                        if (navigationTab.tabName === 'Remaining Life Limit') {
+                        if (navigationTab.tabName === 'Remaining Life Limit' 
+                            || navigationTab.tabName === 'Target Conditional Goals' 
+                            || navigationTab.tabName === 'Deficient Conditional Goal' 
+                            || navigationTab.tabName === 'Calculated Attribute') {
                             navigationTab['visible'] = vm.isAdmin;
                         }
 
