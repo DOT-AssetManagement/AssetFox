dafadde5766e05952dcc7396f90a0bb3ff3338ec
Tyler Daws <tyler.j.daws@gmail.com>
2022-03-10 13:34:57 -0500

    removed non-existent action from budget priority editor

--- BridgeCareApp/VuejsApp/src/components/budget-priority-editor/BudgetPriorityEditor.vue
+++ BridgeCareApp/VuejsApp/src/components/budget-priority-editor/BudgetPriorityEditor.vue
@@ -272,7 +272,6 @@ export default class BudgetPriorityEditor extends Vue {
                 }
 
                 vm.hasScenario = true;
-                vm.getScenarioSimpleBudgetDetailsAction({ scenarioId: vm.selectedScenarioId });
                 vm.getScenarioBudgetPrioritiesAction(vm.selectedScenarioId);
             }
         });
