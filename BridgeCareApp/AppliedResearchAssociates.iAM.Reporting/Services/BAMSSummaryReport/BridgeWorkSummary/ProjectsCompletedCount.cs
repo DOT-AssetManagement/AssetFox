5e6d5883e1359597f98188b16d1b7eac217fcb96
Amruta Borgaonkar <aborgaonkar@ara.com>
2023-04-12 16:44:39 -0500

    updated

--- BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/BridgeWorkSummary/ProjectsCompletedCount.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/BridgeWorkSummary/ProjectsCompletedCount.cs
@@ -267,7 +267,7 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Bri
                         worksheet.Cells[row, column].Value = culvertCount;
 
                         var keyItem = treatment.Name + "_" + yearlyValues.Key;
-                        if (projectRowNumberModel.TreatmentsCount.ContainsKey(keyItem) == false)
+                        if (!projectRowNumberModel.TreatmentsCount.ContainsKey(keyItem))
                         {
                             projectRowNumberModel.TreatmentsCount.Add(keyItem, row);
                         }
@@ -276,7 +276,6 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Bri
                             var warningMessage = "Item key '" + keyItem + "' already exists in the list";
                             if (_warnings.Contains(warningMessage) == false) { _warnings.Add(warningMessage); }
                         }                        
-
                         row++;
 
                         //exclude No Treatment from total
