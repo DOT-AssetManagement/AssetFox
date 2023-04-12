315d3c7d822f5c09870022000d7ec22ff753937a
Amruta Borgaonkar <aborgaonkar@ara.com>
2023-04-12 16:21:45 -0500

    Bug fix for handling duplicate treatmentName_year pairs for Culverts

--- BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/BridgeWorkSummary/ProjectsCompletedCount.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/BridgeWorkSummary/ProjectsCompletedCount.cs
@@ -265,7 +265,18 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Bri
                     {
                         yearlyValues.Value.TryGetValue(treatment.Name, out var culvertCount);
                         worksheet.Cells[row, column].Value = culvertCount;
-                        projectRowNumberModel.TreatmentsCount.Add(treatment.Name + "_" + yearlyValues.Key, row);
+
+                        var keyItem = treatment.Name + "_" + yearlyValues.Key;
+                        if (projectRowNumberModel.TreatmentsCount.ContainsKey(keyItem) == false)
+                        {
+                            projectRowNumberModel.TreatmentsCount.Add(keyItem, row);
+                        }
+                        else
+                        {
+                            var warningMessage = "Item key '" + keyItem + "' already exists in the list";
+                            if (_warnings.Contains(warningMessage) == false) { _warnings.Add(warningMessage); }
+                        }                        
+
                         row++;
 
                         //exclude No Treatment from total
