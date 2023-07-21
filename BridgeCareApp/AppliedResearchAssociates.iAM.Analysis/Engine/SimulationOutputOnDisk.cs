28c44a9ee4f6331079399f36c01c542e749d424c
Amruta Borgaonkar <aborgaonkar@ara.com>
2023-07-21 15:06:33 -0500

    Fixed PR comments.

--- BridgeCareApp/AppliedResearchAssociates.iAM.Analysis/Engine/SimulationOutputOnDisk.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Analysis/Engine/SimulationOutputOnDisk.cs
@@ -69,6 +69,11 @@ namespace AppliedResearchAssociates.iAM.Analysis.Engine
 
         private readonly string MainFilePath;
 
+        ~SimulationOutputOnDisk()
+        {
+            TryDeleteFolder();
+        }
+
         private static void Serialize<T>(T t, string path)
         {
             using var writer = File.CreateText(path);
