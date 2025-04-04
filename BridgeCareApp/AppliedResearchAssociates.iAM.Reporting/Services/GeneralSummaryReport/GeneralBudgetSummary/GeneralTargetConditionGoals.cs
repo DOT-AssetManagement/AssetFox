03e343615b67a583f7a5cb2d8c18479c28faa344
Amruta Borgaonkar <aborgaonkar@ara.com>
2025-04-04 11:43:08 -0500

    GeneralSummaryReport

--- BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/GeneralSummaryReport/GeneralBudgetSummary/GeneralTargetConditionGoals.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/GeneralSummaryReport/GeneralBudgetSummary/GeneralTargetConditionGoals.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AppliedResearchAssociates.iAM.Analysis.Engine;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using AppliedResearchAssociates.iAM.DTOs;
