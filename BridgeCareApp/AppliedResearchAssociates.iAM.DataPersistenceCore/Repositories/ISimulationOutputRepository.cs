fa253963fa7678f0d32c38d9e08b49369b0808bb
Amruta Borgaonkar <aborgaonkar@ara.com>
2025-04-04 17:10:27 -0500

    Removed methods added to try out a way

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/ISimulationOutputRepository.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/ISimulationOutputRepository.cs
@@ -17,31 +17,13 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories
         SimulationOutput GetSimulationOutputViaRelation(Guid simulationId, ILog loggerForUserInfo = null, ILog loggerForTechincalInfo = null, List<AttributeDTO> attributeDtos = null);
 
         SimulationOutputEntity GetSimulationOutputWithoutAssetSummariesOrYearContents(Guid simulationId);
-
-        public SimulationOutput GetSimulationOutputSimpleViaRelation(Guid simulationId,
-            List<SimulationYearDetailEntity> cacheYears,
-            Dictionary<Guid, string> attributeNameLookup,
-            SimulationOutputEntity entityWithoutAssetSummariesOrYearContents,
-            ILog loggerForUserInfo = null,
-            ILog loggerForTechinalInfo = null);
-
-        public List<AssetSummaryDetail> GetSimulationOutputInitialAssetSummariesViaRelation(Guid simulationOutputId,
-            Dictionary<Guid, string> attributeNameLookup,
-            Dictionary<Guid, string> assetNameLookup,
-            ILog loggerForUserInfo = null,
-            ILog loggerForTechinalInfo = null);
-
-        public List<SimulationYearDetail> GetSimulationOutputYearsViaRelation(Guid simulationId,
-            List<SimulationYearDetailEntity> cacheYears,
-            Dictionary<Guid, string> attributeNameLookup,
-            Dictionary<Guid, string> assetNameLookup,
-            ILog loggerForUserInfo = null,
-            ILog loggerForTechinalInfo = null);        
-
+                
         SimulationOutput GetSimulationOutputViaJson(Guid simulationId);
+
         void CreateSimulationOutputViaJson(Guid simulationId, SimulationOutput simulationOutput);
 
         void ConvertSimulationOutpuFromJsonTorelational(Guid simulationId, CancellationToken? cancellationToken = null, IWorkQueueLog queueLogger = null);
+
         void DeleteScenarioOutputsWithingDaterange(DateTime? startDate, DateTime endDate, CancellationToken token);
     }
 }
