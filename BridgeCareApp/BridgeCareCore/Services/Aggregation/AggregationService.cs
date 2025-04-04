dc282626c0d966da20377196ecb814cdeb8fbc5e
Tyler Daws <tyler.j.daws@gmail.com>
2025-04-04 16:21:56 -0400

    initial batching/iteration changes

--- BridgeCareApp/BridgeCareCore/Services/Aggregation/AggregationService.cs
+++ BridgeCareApp/BridgeCareCore/Services/Aggregation/AggregationService.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.Common;
 using AppliedResearchAssociates.iAM.Data;
 using AppliedResearchAssociates.iAM.Data.Aggregation;
 using AppliedResearchAssociates.iAM.Data.Attributes;
@@ -14,6 +16,8 @@ using AppliedResearchAssociates.iAM.Data.Networking;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using AppliedResearchAssociates.iAM.DTOs;
+using Microsoft.Extensions.DependencyInjection;
+using NuGet.ContentModel;
 using Writer = System.Threading.Channels.ChannelWriter<BridgeCareCore.Services.Aggregation.AggregationStatusMemo>;
 
 namespace BridgeCareCore.Services.Aggregation
@@ -21,9 +25,11 @@ namespace BridgeCareCore.Services.Aggregation
     public class AggregationService : IAggregationService
     {
         private readonly IUnitOfWork _unitOfWork;
-        public AggregationService(IUnitOfWork unitOfWork)
+        private readonly ILog _log;
+        public AggregationService(IUnitOfWork unitOfWork, ILog log)
         {
             _unitOfWork = unitOfWork;
+            _log = log ?? throw new ArgumentNullException(nameof(log));
         }
 
         /// <summary>AggregationState can be just new AggregationState() object. Purpose is to allow calling class to access the state.</summary>
@@ -36,6 +42,10 @@ namespace BridgeCareCore.Services.Aggregation
 
             await Task.Run(() =>
             {
+                var stopwatch = Stopwatch.StartNew();
+
+                _log.Information($"Starting Aggregation.");
+
                 try
                 {
                     _unitOfWork.BeginTransaction();
@@ -87,6 +97,9 @@ namespace BridgeCareCore.Services.Aggregation
                     // create list of attribute data from configuration attributes (exclude attributes
                     // that don't have command text as there will be no way to select data for them from
                     // the data source)
+                    stopwatch.Stop();
+                    _log.Information($"Finished prework agg. {stopwatch.ElapsedMilliseconds}ms");
+                    stopwatch.Start();
                     try
                     {
                         foreach (var attribute in configurationAttributes)
@@ -113,6 +126,9 @@ namespace BridgeCareCore.Services.Aggregation
                         throw new Exception(e.StackTrace);
                     }
 
+                    stopwatch.Stop();
+                    _log.Information($"Finished attribute data. {stopwatch.ElapsedMilliseconds}ms");
+                    stopwatch.Start();
                     // get the attribute ids for assigned data that can be deleted (attribute is present
                     // in the data source and meta data file)
                     attributeIdsToBeUpdatedWithAssignedData = configurationAttributes.Select(_ => _.Id)
@@ -121,7 +137,7 @@ namespace BridgeCareCore.Services.Aggregation
                     var aggregatedResults = new List<IAggregatedResult>();
 
                     var totalAssets = (double)maintainableAssets.Count;
-                    var i = 0.0;
+                    var count = 0.0;
 
                     var directory = Directory.GetCurrentDirectory();
                     var path = Path.Combine(directory, "Logs");
@@ -136,57 +152,114 @@ namespace BridgeCareCore.Services.Aggregation
                     }
                     state.Status = "Aggregating";
                     _unitOfWork.NetworkRepo.UpsertNetworkRollupDetail(networkId, state.Status);
+
+                    stopwatch.Stop();
+                    _log.Information($"Starting agg step. {stopwatch.ElapsedMilliseconds}ms");
+                    stopwatch.Start();
+
+                    var attributeDataByLocationIdentifier = attributeData
+                        .GroupBy(d => d.Location.LocationIdentifier)
+                        .ToDictionary(g => g.Key, g => g.ToList());
+
                     // loop over maintainable assets and remove assigned data that has an attribute id
                     // in attributeIdsToBeUpdatedWithAssignedData then assign the new attribute data
                     // that was created
-                    foreach (var maintainableAsset in maintainableAssets)
+                    const int CHUNK_SIZE = 10000; // Adjust based on your memory constraints
+                    var assetCount = maintainableAssets.Count;
+
+                    for (int i = 0; i < assetCount; i += CHUNK_SIZE)
                     {
-                        if (cancellationToken != null && cancellationToken.Value.IsCancellationRequested)
+                        // Get a chunk of assets to process
+                        var assetChunk = maintainableAssets.Skip(i).Take(CHUNK_SIZE).ToList();
+                        var chunkResults = new List<IAggregatedResult>();
+
+                        foreach (var maintainableAsset in assetChunk)
                         {
-                            _unitOfWork.Rollback();
-                            return;
-                        }
-                        if (i % 500 == 0)
-                        {
-                            state.Percentage = Math.Round(i / totalAssets * 100, 1);
-                        }
-                        i++;
-                        maintainableAsset.AssignedData.RemoveAll(_ =>
-                            attributeIdsToBeUpdatedWithAssignedData.Contains(_.Attribute.Id));
-                        //List<DatumLog> unmatchedDatum = maintainableAsset.AssignAttributeData(attributeData);
-                        maintainableAsset.AssignAttributeData(attributeData);
-                        try
-                        {
-                            // aggregate numeric data
-                            if (maintainableAsset.AssignedData.Any(_ => _.Attribute.DataType == "NUMBER"))
+                            string locationIdentifier = maintainableAsset.Location.LocationIdentifier;
+
+                            if (cancellationToken != null && cancellationToken.Value.IsCancellationRequested)
                             {
-                                aggregatedResults.AddRange(maintainableAsset.AssignedData
-                                    .Where(_ => _.Attribute.DataType == "NUMBER")
-                                    .Select(_ => _.Attribute).Distinct()
-                                    .Select(_ =>
-                                        maintainableAsset.GetAggregatedValuesByYear(_,
-                                            AggregationRuleFactory.CreateNumericRule(_)))
-                                    .ToList());
+                                _unitOfWork.Rollback();
+                                return;
+                            }
+                            if (count % 500 == 0)
+                            {
+                                state.Percentage = Math.Round(i / totalAssets * 100, 1);
+                            }
+                            count++;
+                            maintainableAsset.AssignedData.RemoveAll(_ =>
+                                attributeIdsToBeUpdatedWithAssignedData.Contains(_.Attribute.Id));
+                            //List<DatumLog> unmatchedDatum = maintainableAsset.AssignAttributeData(attributeData);
+                            //maintainableAsset.AssignAttributeData(attributeData);
+
+                            if (attributeDataByLocationIdentifier.TryGetValue(locationIdentifier, out var matchingData))
+                            {
+                                // Filter just to make sure they're the right type
+                                var correctTypeData = matchingData.Where(d => d.Location.GetType() == maintainableAsset.Location.GetType());
+                                maintainableAsset.AssignedData.AddRange(correctTypeData);
                             }
 
-                            //aggregate text data
-                            if (maintainableAsset.AssignedData.Any(_ => _.Attribute.DataType == "STRING"))
+                            try
                             {
-                                aggregatedResults.AddRange(maintainableAsset.AssignedData
-                                    .Where(_ => _.Attribute.DataType == "STRING")
-                                    .Select(_ => _.Attribute).Distinct()
-                                    .Select(_ => maintainableAsset.GetAggregatedValuesByYear(_,
-                                        AggregationRuleFactory.CreateTextRule(_))).ToList());
+                                // aggregate numeric data
+                                if (maintainableAsset.AssignedData.Any(_ => _.Attribute.DataType == "NUMBER"))
+                                {
+                                    chunkResults.AddRange(maintainableAsset.AssignedData
+                                        .Where(_ => _.Attribute.DataType == "NUMBER")
+                                        .Select(_ => _.Attribute).Distinct()
+                                        .Select(_ =>
+                                            maintainableAsset.GetAggregatedValuesByYear(_,
+                                                AggregationRuleFactory.CreateNumericRule(_)))
+                                        .ToList());
+                                }
+
+                                //aggregate text data
+                                if (maintainableAsset.AssignedData.Any(_ => _.Attribute.DataType == "STRING"))
+                                {
+                                    chunkResults.AddRange(maintainableAsset.AssignedData
+                                        .Where(_ => _.Attribute.DataType == "STRING")
+                                        .Select(_ => _.Attribute).Distinct()
+                                        .Select(_ => maintainableAsset.GetAggregatedValuesByYear(_,
+                                            AggregationRuleFactory.CreateTextRule(_))).ToList());
+                                }
                             }
+                            catch (Exception e)
+                            {
+                                var networkName = _unitOfWork.NetworkRepo.GetNetworkNameOrId(networkId);
+                                var broadcastError = $"Error: Creating aggregation rule(s) for the attributes for {networkName}:: {e.Message}";
+                                WriteError(writer, broadcastError);
+                                throw;
+                            }
+                        }
+
+                        try
+                        {
+                            _unitOfWork.AggregatedResultRepo.AddAggregatedResults(chunkResults);
                         }
                         catch (Exception e)
                         {
                             var networkName = _unitOfWork.NetworkRepo.GetNetworkNameOrId(networkId);
-                            var broadcastError = $"Error: Creating aggregation rule(s) for the attributes for {networkName}:: {e.Message}";
+                            var broadcastError = $"Error while adding Aggregated results for {networkName} -  {e.Message}";
                             WriteError(writer, broadcastError);
-                            throw;
+                            isError = true;
+                            state.ErrorMessage = e.Message;
+                            throw new Exception(e.StackTrace);
                         }
+
+                        chunkResults.Clear();
+                        assetChunk.Clear();
+
+                        // Force garbage collection if memory pressure is high
+                        if (i % (CHUNK_SIZE * 10) == 0)
+                        {
+                            GC.Collect();
+                        }
+
+                        state.Percentage = Math.Round((double)i / assetCount * 100, 1);
+                        WriteState(writer, state);
+
                     }
+
                     if (cancellationToken != null && cancellationToken.Value.IsCancellationRequested)
                     {
                         _unitOfWork.Rollback();
@@ -194,6 +267,10 @@ namespace BridgeCareCore.Services.Aggregation
                     }
                     state.Status = "Saving";
                     _unitOfWork.NetworkRepo.UpsertNetworkRollupDetail(networkId, state.Status);
+
+                    stopwatch.Stop();
+                    _log.Information($"Finished Agg Results. Start Att Datum. {stopwatch.ElapsedMilliseconds}ms");
+                    stopwatch.Start();
 
                     //streamWriter.Close();
 
@@ -210,6 +287,9 @@ namespace BridgeCareCore.Services.Aggregation
                         state.ErrorMessage = e.Message;
                         throw new Exception(e.StackTrace);
                     }
+                    stopwatch.Stop();
+                    _log.Information($"Finished Att datum. {stopwatch.ElapsedMilliseconds}ms");
+                    stopwatch.Start();
                     try
                     {
                         _unitOfWork.MaintainableAssetRepo.UpdateMaintainableAssetsSpatialWeighting(maintainableAssets);
@@ -218,19 +298,6 @@ namespace BridgeCareCore.Services.Aggregation
                     {
                         var networkName = _unitOfWork.NetworkRepo.GetNetworkNameOrId(networkId);
                         var broadcastError = $"Error while Updating MaintainableAssets SpatialWeighting for {networkName} -  {e.Message}";
-                        WriteError(writer, broadcastError);
-                        isError = true;
-                        state.ErrorMessage = e.Message;
-                        throw new Exception(e.StackTrace);
-                    }
-                    try
-                    {
-                        _unitOfWork.AggregatedResultRepo.AddAggregatedResults(aggregatedResults);
-                    }
-                    catch (Exception e)
-                    {
-                        var networkName = _unitOfWork.NetworkRepo.GetNetworkNameOrId(networkId);
-                        var broadcastError = $"Error while adding Aggregated results for {networkName} -  {e.Message}";
                         WriteError(writer, broadcastError);
                         isError = true;
                         state.ErrorMessage = e.Message;
@@ -251,6 +318,9 @@ namespace BridgeCareCore.Services.Aggregation
                         state.Percentage = 0;
                         WriteState(writer, state);
                     }
+                    stopwatch.Stop();
+                    _log.Information($"Finished Agg. {stopwatch.ElapsedMilliseconds}ms");
+
                 }
                 catch
                 {
