8cc8aa75997aa00681e4f71a118ba2f597c1b9cb
Amruta Borgaonkar <aborgaonkar@ara.com>
2025-04-15 17:04:45 -0500

    Initial work - in progress

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Models/UserDefinedReport/UserDefinedReportRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AppliedResearchAssociates.iAM.Reporting.Models
+{
+    public class UserDefinedReportRequestModel
+    {
+        // Filters
+
+        public List<string> Attributes { get; set; } // InitialAssetSummaries' AssetSummaryDetailValuesIntId details with given Attributes
+
+        public List<int> Years { get; set; }
+
+        // Show/Hide (next level report)
+        public bool DisplayBudgets { get; set; } = true;
+                
+        public bool DisplayDeficientConditionGoals { get; set; } = true;        
+                
+        public bool DisplayTargetConditionGoals { get; set; } = true;
+                
+        public bool DisplayAssets { get; set; } = true;
+        // Assets => further drill down for show/hide can be planned as required...
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/UserDefinedReport/YearTab.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedResearchAssociates.iAM.Reporting.Services.UserDefinedReport
+{
+    internal class YearTab
+    {
+    }
+}
