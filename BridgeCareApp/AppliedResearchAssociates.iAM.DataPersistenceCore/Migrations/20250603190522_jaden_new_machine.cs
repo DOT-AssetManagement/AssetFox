4fd35e3517a72efab3aa16231f1620fbe1c6da15
Jaden Nobles <jnobles@ara.com>
2025-06-09 09:21:07 -0400

    Initial Setup

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20250603190522_jaden_new_machine.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Migrations
+{
+    public partial class jaden_new_machine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
