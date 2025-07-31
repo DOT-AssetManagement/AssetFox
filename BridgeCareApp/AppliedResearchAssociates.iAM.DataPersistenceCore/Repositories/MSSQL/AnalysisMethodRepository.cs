2b9a379b5410420c9187cd0a8b62a7c3dd512350
William Jockusch <william.jockusch@sc-binc.com>
2025-07-31 10:30:27 -0500

    When saving the analysis method, if there are no criteria, get the asset count from the db.

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/AnalysisMethodRepository.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/AnalysisMethodRepository.cs
@@ -124,6 +124,14 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL
                 throw new RowNotInTableException("No simulation was found for the given scenario.");
             }
 
+            if (string.IsNullOrWhiteSpace(dto.CriterionLibrary.MergedCriteriaExpression) && dto.LastKnownAssetCount <= 0)
+            {
+                var simulation = _unitOfWork.Context.Simulation
+                    .AsNoTracking()
+                    .FirstOrDefault(s => s.Id == simulationId);
+                dto.LastKnownAssetCount = _unitOfWork.Context.MaintainableAsset.Count(ma => ma.NetworkId == simulation.NetworkId);
+            }
+
             AttributeEntity attributeEntity = null;
 
             if (!string.IsNullOrEmpty(dto.Attribute))
