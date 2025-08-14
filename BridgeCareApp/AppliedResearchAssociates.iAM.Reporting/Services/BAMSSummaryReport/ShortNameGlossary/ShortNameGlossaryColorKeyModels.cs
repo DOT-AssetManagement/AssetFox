1e7d4d628d5e5357f0dc24943386386c3e3a74af
Amruta Borgaonkar <aborgaonkar@ara.com>
2025-08-14 17:35:11 -0500

    Updated BAMS legend rows spacing

--- BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/ShortNameGlossary/ShortNameGlossaryColorKeyModels.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/ShortNameGlossary/ShortNameGlossaryColorKeyModels.cs
@@ -36,6 +36,13 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Sho
                     ExcelValueModels.String("Bridge replacement is cash flowed over 3 years.")
                     ),
                 TwoByOneRow(ExcelValueModels.Nothing),
+                TwoByOneRow(ExcelValueModels.Nothing),
+                TwoByOneRow(ExcelValueModels.Nothing),
+                TwoByOneRow(ExcelValueModels.Nothing),
+                TwoByOneRow(ExcelValueModels.Nothing),
+                TwoByOneRow(ExcelValueModels.Nothing),
+                TwoByOneRow(ExcelValueModels.Nothing),
+                TwoByOneRow(ExcelValueModels.Nothing),
                 CenteredLongHeader("This software is provided on “as is” basis, and by using the software you acknowledge and agree that you are doing so at your own risk. While reasonable efforts have been made to ensure the accuracy and reliability of the software, no guarantee is made regarding the correctness, completeness, or suitability of any models, predictions or results it generates. Under no circumstances shall the software developers or distributors be held liable for any loss, damage, or other consequences arising from the use of the software or its outputs. Users assume full responsibility for any decisions made based on software use.")
         );
 
@@ -48,7 +55,7 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Sho
                         ExcelStyleModels.Bold,
                         ExcelStyleModels.HorizontalCenter,
                         ExcelStyleModels.WrapText),
-                    Size = new ExcelRangeSize(10, 6)
+                    Size = new ExcelRangeSize(8, 7)
                 });
 
         private static ExcelRowModel CenteredHeader(string text)
@@ -85,6 +92,5 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Sho
                 Content = content,
                 Size = new ExcelRangeSize(2, 1)
             };
-
     }
 }
